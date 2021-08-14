879618a823b1f610938fcfa0d32b94691e4bfe53
Curtis Kettler <curtiskettler@gmail.com>
2021-08-14 13:09:52 -0500

    working on toggle photo

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
+
 function GalleryItem ({data, photo, photoLikes}) {
+
+    const [photoOrDescription, setPhotoOrDescription] = useState(true);
 
     const photoLiked = () => {
         photoLikes(data.id)
     }
 
+    const togglePhoto = () => {
+        console.log('SWITCH');
+
+    }
+
     return (
         <>
         <div>
-            <img src={photo.path}/>
+            <img src={photo.path} onClick={togglePhoto}/>
             <div>
                 <button onClick={photoLiked}>Like!</button>
                 <h3>{photo.likes} likes</h3>
