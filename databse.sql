0c917554b70b7a3c06d305eab4086c8abea5a163
Curtis Kettler <curtiskettler@gmail.com>
2021-08-14 14:05:53 -0500

    databse created

---
+++ databse.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "gallery"(
+"id" SERIAL PRIMARY KEY,
+"path" VARCHAR(500),
+"description" VARCHAR(240),
+"likes" INTEGER
+);
+
+INSERT INTO "gallery"("path", "description", "likes")
+VALUES('images/goat_small.jpg', 'Photo of a goat taken at Glacier National Park.', 0);
+
