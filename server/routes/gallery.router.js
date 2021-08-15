01f96b73936103a0ebcb18663d9e80e47f279eb8
Curtis Kettler <curtiskettler@gmail.com>
2021-08-15 09:56:54 -0500

    server POST written

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -3,6 +3,45 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 
+// GET Route
+router.get('/', (req, res) => {
+    const sqlQuery =`
+        SELECT * FROM "gallery"
+        ORDER BY "id" ASC
+    `;
+    pool.query(sqlQuery)
+        .then((result) => {
+            console.log('GET data from db', result);
+            res.send(result.rows);
+        }).catch((error) => {
+            console.log('GET error', error);
+            res.sendStatus(500);
+        });
+}); // END GET Route
+
+// POST Route
+router.post('/', (req, res) => {
+    const sqlQuery =`
+        INSERT INTO "gallery"
+        ("path", "description", "likes")
+        VALUES ($1, $2, $3)
+    `;
+    const sqlParams = [
+        req.body.path,
+        req.body.description,
+        req.body.likes
+    ];
+    pool.query(sqlQuery, sqlParams)
+        .then((result) => {
+            console.log('POST photo working', req.body);
+            res.sendStatus(201);
+        }).catch((error) => {
+            console.log('POST error', error);
+            res.sendStatus(500);
+        });
+});
+
+
 // PUT Route
 router.put('/like/:id', (req, res) => {
     const sqlQuery =`
@@ -23,20 +62,6 @@ router.put('/like/:id', (req, res) => {
         });
 }); // END PUT Route
 
-// GET Route
-router.get('/', (req, res) => {
-    const sqlQuery =`
-        SELECT * FROM "gallery"
-        ORDER BY "id" ASC
-    `;
-    pool.query(sqlQuery)
-        .then((result) => {
-            console.log('GET data from db', result);
-            res.send(result.rows);
-        }).catch((error) => {
-            console.log('GET error', error);
-            res.sendStatus(500);
-        });
-}); // END GET Route
+
 
 module.exports = router;
